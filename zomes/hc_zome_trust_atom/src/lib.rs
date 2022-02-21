296bb455d2d0621c66fc645e21b4bd883efb9a6e
dauphin3 <45241372+dauphin3@users.noreply.github.com>
2022-02-20 21:11:00 -0500

    TrustAtom link tag has optional extra entry hash

--- zomes/hc_zome_trust_atom/src/lib.rs
+++ zomes/hc_zome_trust_atom/src/lib.rs
@@ -11,17 +11,24 @@
 // #![warn(clippy::cargo)]
 
 use hdk::prelude::*;
+use std::collections::BTreeMap;
 
 // public for sweettest; TODO can we fix this:
 pub mod trust_atom;
 pub use crate::trust_atom::*;
-
 pub mod test_helpers;
-// pub use crate::test_helpers;
 
-entry_defs![StringTarget::entry_def()];
+entry_defs![test_helpers::StringTarget::entry_def(), Extra::entry_def()];
 
 // INPUT TYPES
+
+#[derive(Serialize, Deserialize, SerializedBytes, Debug, Clone)]
+pub struct TrustAtomInput {
+  pub target: EntryHash, // TODO maybe target_entry_hash?
+  pub content: Option<String>,
+  pub value: Option<String>,
+  pub extra: Option<BTreeMap<String, String>>,
+}
 
 #[derive(Serialize, Deserialize, SerializedBytes, Debug, Clone)]
 pub struct QueryInput {
@@ -29,7 +36,7 @@ pub struct QueryInput {
   pub target: Option<EntryHash>,
   pub content_full: Option<String>,
   pub content_starts_with: Option<String>,
-  pub min_rating: Option<String>,
+  pub value_starts_with: Option<String>,
 }
 
 #[derive(Serialize, Deserialize, SerializedBytes, Debug, Clone)]
@@ -37,14 +44,28 @@ pub struct QueryMineInput {
   pub target: Option<EntryHash>,
   pub content_full: Option<String>,
   pub content_starts_with: Option<String>,
-  pub min_rating: Option<String>,
+  pub value_starts_with: Option<String>,
 }
 
 // ZOME API FUNCTIONS
 
 #[hdk_extern]
-pub fn create_trust_atom(input: TrustAtomInput) -> ExternResult<()> {
-  trust_atom::create(input.target, &input.content, &input.value, input.attributes)
+pub fn create_trust_atom(input: TrustAtomInput) -> ExternResult<TrustAtom> {
+  let trust_atom = trust_atom::create(input.target, input.content, input.value, input.extra)?;
+  Ok(trust_atom)
+}
+
+#[hdk_extern]
+#[allow(clippy::needless_pass_by_value)]
+pub fn get_extra(entry_hash: EntryHash) -> ExternResult<Extra> {
+  let extra = trust_atom::get_extra(&entry_hash)?;
+  Ok(extra)
+}
+
+#[hdk_extern]
+pub fn calc_extra_hash(input: Extra) -> ExternResult<EntryHash> {
+  let hash = trust_atom::calc_extra_hash(input)?;
+  Ok(hash)
 }
 
 #[hdk_extern]
@@ -54,7 +75,7 @@ pub fn query(input: QueryInput) -> ExternResult<Vec<TrustAtom>> {
     input.target,
     input.content_full,
     input.content_starts_with,
-    input.min_rating,
+    input.value_starts_with,
   )
 }
 
@@ -64,15 +85,14 @@ pub fn query_mine(input: QueryMineInput) -> ExternResult<Vec<TrustAtom>> {
     input.target,
     input.content_full,
     input.content_starts_with,
-    input.min_rating,
+    input.value_starts_with,
   )
 }
-
 // TEST HELPERS
 
 #[hdk_extern]
 pub fn create_string_target(input: String) -> ExternResult<EntryHash> {
-  crate::trust_atom::create_string_target(input)
+  crate::test_helpers::create_string_target(input)
 }
 
 #[hdk_extern]
