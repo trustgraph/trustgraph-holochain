23e1a4608e4e501697e6e43d11112c4dbb2baaa7
dauphin3 <45241372+dauphin3@users.noreply.github.com>
2022-03-12 09:20:47 -0500

    tests

--- zomes/hc_zome_trust_graph/src/lib.rs
+++ zomes/hc_zome_trust_graph/src/lib.rs
@@ -4,10 +4,18 @@ use hdk::prelude::holo_hash::*;
 pub mod trust_graph;
 pub use crate::trust_graph::*;
 
+// INPUT TYPES
+
+#[derive(Serialize, Deserialize, SerializedBytes, Debug, Clone)]
+pub struct Input {
+  pub agents: Vec<EntryHash>,
+  pub tag_filter: Option<LinkTag>,
+}
+
 // ZOME API FUNCTIONS
 
 #[hdk_extern]
-pub fn create_trust_graph() -> ExternResult<TrustGraph> {
+pub fn create_trust_graph(input: Input) -> ExternResult<TrustGraph> {
   let trust_graph = trust_graph::TrustGraph::create()?;
   Ok(trust_graph)
 }
