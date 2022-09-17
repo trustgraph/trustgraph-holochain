b98007f6c5d25344e4b812486de42acc54c66e03
Zeek <sil3ntsc0pe@yahoo.com>
2022-09-17 04:42:26 -0400

    bump hc 162 hdk 152 hdi 1.2 holo_hash 32

--- default.nix
+++ default.nix
@@ -1,21 +1,14 @@
-{
-  holonixPath ?  builtins.fetchTarball { url = "https://github.com/holochain/holonix/archive/d15633710a8d4349dc0ff03b7b47ad01eb9f2433.tar.gz"; }
-}:
-
 let
+  holonixPath = (import ./nix/sources.nix).holonix; # points to the current state of the Holochain repository
   holonix = import (holonixPath) {
-    rustVersion = {
-      track = "stable";
-      version = "1.57.0";
-    };
+    holochainVersionId = "v0_0_162"; # specifies the Holochain version
   };
   nixpkgs = holonix.pkgs;
 in nixpkgs.mkShell {
   inputsFrom = [ holonix.main ];
-  buildInputs = with nixpkgs; [
-    binaryen
-    cargo-release
-    cargo-watch
+  packages = with nixpkgs; [
+    niv
     nodejs-16_x
+    # any additional packages needed for this project, e. g. Nodejs
   ];
 }
