7934cca2334693ef3d4786b83ed24d373627adcc
Zeek <zkringlwe@gmail.com>
2023-05-12 23:57:11 -0600

    validation
    Co-authored-by: Zeek <zkringlwe@gmail.com>

--- zomes/trust_atom_integrity/src/validation.rs
+++ zomes/trust_atom_integrity/src/validation.rs
@@ -1,1 +1,58 @@
+use hdi::prelude::*;
+use crate::{EntryTypes, LinkTypes};
+
+
+
+
+#[hdk_extern]
+pub fn validate_create_trust_atom(op: Op) -> ExternResult<ValidateCallbackResult> {
+
+	match op.flattened::<EntryTypes, LinkTypes>()? {
+
+		FlatOp::RegisterCreateLink {
+			            link_type,
+			            base_address,
+			            target_address,
+			            tag,
+			            action,
+			        } => {
+			            match link_type {
+			                LinkTypes::TrustAtom => {
+			                    
+			                }
+			               
+			                
+			            }
+			        }
+			                
+			            }
+			        }
+		_ => {}
+		
+
+#[hdk_extern]
+pub fn validate_delete_trust_atom(op: Op) -> ExternResult<ValidateCallbackResult> {
+	let agent = agent_info().agent_initial_pubkey?;
+	match op.flattened::<EntryTypes, LinkTypes>()? {
+		
+			FlatOp::RegisterDeleteLink {
+				link_type,
+				base_address,
+				target_address,
+				tag,
+				original_action,
+				action,
+			} => {
+				match link_type {
+					LinkTypes::TrustAtom => {
+						
+					}
+					
+				}
+			}
+		_ => {}
+	}
+
+
+}
 
