70a207356926d27d2c786322952253d811ea398f
Zeek <zkringlwe@gmail.com>
2023-05-20 00:03:39 -0600

    create and delete validation
    Co-authored-by: Zeek <zkringlwe@gmail.com>

--- zomes/trust_atom_integrity/src/validation.rs
+++ zomes/trust_atom_integrity/src/validation.rs
@@ -1,5 +1,9 @@
 use crate::{EntryTypes, LinkTypes};
 use hdi::prelude::*;
+use hdk::prelude::agent_info;
+
+const UNICODE_NUL_STR: &str = "\u{0}";
+const UNICODE_T_CHAR: &str = "\u{0}";
 
 #[hdk_extern]
 pub fn validate(op: Op) -> ExternResult<ValidateCallbackResult> {
@@ -10,38 +14,68 @@ pub fn validate(op: Op) -> ExternResult<ValidateCallbackResult> {
       tag,
       link_type,
       action,
-		} => match link_type {
-			match null_byte_count {
-				4 => // Ok(ValidateCallbackResult::Valid)
-			 	null_bytes =>
-						Ok(ValidateCallbackResult::Invalid(String::from(
-							"Invalid link tag!  Expected 4 null bytes, but got {}", null_bytes,
-						)))
+		} => {
+            if base_address != agent_info()?.agent_initial_pubkey.into() { 
+                    return Ok(ValidateCallbackResult::Invalid(
+                		"Creator must be same as base_address".to_string()))
+                }
+            if base_address != AnyLinkableHash::from(action.author) { 
+                return Ok(ValidateCallbackResult::Invalid(
+					      "Creator must be same as base_address".to_string()))
+            }
+            
+			let link_tag = String::from_utf8(tag.into_inner());
 
-		},
-			// from https://github.com/holochain-immersive/private-publication.git
-			// LinkTypes::PathToPost => {
-			// 		validate_create_link_all_posts(action, base_address, target_address, tag)
-			// }
-    }
-    _ => Ok(ValidateCallbackResult::Valid),
-  }
-}
+      match &link_tag {
+        Ok(tag) => {
+            match tag.matches(UNICODE_NUL_STR).count() {
+          4 => { let chunks: Vec<&str> = tag.split(UNICODE_NUL_STR).collect(); 
+                // debug!("{:?}", chunks);
+                // if chunks[0].as_bytes() != [0xC5][0xA6][0x21][0x92] || [0xC5][0xA6][0x21][0xA9] {
+                //   return Err(wasm_error!(WasmErrorInner::Guest("LinkTag format error - prefix symbol incorrect".to_string())))
+                // }
+                if chunks[1].as_bytes().len() > 900 {
+                  if chunks[4] == "" {
+                    return Err(wasm_error!(WasmErrorInner::Guest("LinkTag format error - content > 900 bytes must have extra entry hash".to_string())))
+                  }  
+                }
+                if chunks[2].chars().count() > 12 {
+                  return Err(wasm_error!(WasmErrorInner::Guest("LinkTag format error - rating must be <= 12 chars".to_string())))
+                }
+                if chunks[3].chars().count() != 9 {
+                  return Err(wasm_error!(WasmErrorInner::Guest("LinkTag format error - must have 9 char bucket".to_string())))
+                }
+                else {
+                  return Ok(ValidateCallbackResult::Valid)
+                }
+              },
+          _ => return Ok(ValidateCallbackResult::Invalid(String::from(
+              "Invalid link tag! Expected 4 null bytes.",
+            ))),
+            };
+        },
+        Err(_) => return Err(wasm_error!(WasmErrorInner::Guest("LinkTag format error".to_string()))),
+          
+      }
 
-#[hdk_extern]
-pub fn validate_delete_trust_atom(op: Op) -> ExternResult<ValidateCallbackResult> {
-  let agent = agent_info().agent_initial_pubkey?;
-  match op.flattened::<EntryTypes, LinkTypes>()? {
-    FlatOp::RegisterDeleteLink {
-      link_type,
-      base_address,
-      target_address,
-      tag,
-      original_action,
-      action,
-    } => match link_type {
-      LinkTypes::TrustAtom => {}
+
+
     },
-    _ => {}
-  }
+	OpType::RegisterDeleteLink {
+		link_type,
+		base_address,
+		target_address,
+		tag,
+		original_action,
+		action,
+		} => {
+			if action.author != original_action.author {
+				return Ok(ValidateCallbackResult::Invalid(
+					"Only the original author can delete Trust Atom".to_string(),
+				));
+			}
+			Ok(ValidateCallbackResult::Valid)
+		},
+	_ => { Ok(ValidateCallbackResult::Valid) } // TODO: Exhuast remaining match options
+	}
 }
